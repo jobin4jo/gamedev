f0a5fad3af3597d53d41e5becca1d0e6d6ee66fd
Jobin s <jobins802@gmail.com>
2025-08-15 00:16:05 +0530

    nmanager_dashboard_design

--- src/app/Components/gamemanagerdashboard/gamemanagerdashboard.component.ts
+++ src/app/Components/gamemanagerdashboard/gamemanagerdashboard.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { GameService } from 'src/app/services/game.service';
+import { Modal } from 'bootstrap';
 @Component({
   selector: 'app-gamemanagerdashboard',
   templateUrl: './gamemanagerdashboard.component.html',
@@ -8,27 +9,56 @@ import { Component, OnInit } from '@angular/core';
 export class GamemanagerdashboardComponent implements OnInit {
 
    managerName = 'Manager1';
-  managedGame = 'Game 1';
+  managedGame = 'Game4';
 
   searchTerm = '';
-  searchResults: string[] = [];
+  searchResults: any = [];
   searchClicked = false;
+  IsViewTable:boolean=false;
+  populatedData:any;
+  scoreInput: number | null = null;
+currentGameScore: number | null = null;
 
   onSearch() {
+    debugger;
     this.searchClicked = true;
-
-    const mockUsers = ['player1', 'player2', 'testuser', 'gamerpro'];
-    this.searchResults = mockUsers.filter(user =>
-      user.toLowerCase().includes(this.searchTerm.toLowerCase())
+    this.user.searchUser(this.searchTerm).subscribe(
+      (results: any) => {
+        this.searchResults = results;
+        console.log(this.searchResults);
+      },
+      (error) => {
+        this.searchResults = [];
+       
+      }
     );
   }
 
-  constructor() { }
+  constructor(private user:GameService) { }
 
   ngOnInit(): void {
           
   }
+onView(data:any){
+this.IsViewTable=true;
+this.populatedData=data;
+ const gameObj = data.games.find((g: any) => g.gameName === this.managedGame);
+  this.currentGameScore = gameObj ? gameObj.points : null;
+  console.log('Current Game Score:', this.currentGameScore);
+  this.scoreInput = (this.currentGameScore === null || this.currentGameScore === 0) ? null : this.currentGameScore;
+}
 
- 
+updateScore(){
+  const score = this.scoreInput !== null ? this.scoreInput : 0;
+this.user.UpdateScore(this.populatedData.userNumber, this.managedGame, score).subscribe((res:any)=>{
+  console.log('Score updated successfully:', res);
+    const modalElement = document.getElementById('regModal');
+      if (modalElement) {
+        const modalInstance = new Modal(modalElement);
+        modalInstance.show();
+      }
+      this.IsViewTable = false;
+});
+}
 
 }
